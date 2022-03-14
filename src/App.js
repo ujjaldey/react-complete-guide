4c662713f92fc06e43f64d695da0e0da0c591dfa
Ujjal Dey <ujjaldey@gmail.com>
2022-03-15 00:26:55 +0800

    End #33

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import ExpenseItem from './components/ExpenseItem'
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <h2>This is also visible!</h2>
+      <ExpenseItem></ExpenseItem>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
