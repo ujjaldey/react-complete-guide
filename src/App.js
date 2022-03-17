3ed291b765f8f81518111cb62d2ca960795659a4
Ujjal Dey <ujjaldey@gmail.com>
2022-03-18 00:02:26 +0800

    #39

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from "./components/Expenses";
 
 function App() {
   const expenses = [{
@@ -26,10 +26,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}></ExpenseItem>
+      <Expenses items={expenses} />
     </div>
   );
 }
