5fa13c26cc97c9989e0b8485e5d02373392facc2
Ujjal Dey <ujjaldey@gmail.com>
2022-03-25 01:21:22 +0800

    #67

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -16,6 +16,19 @@ const Expenses = (props) => {
 		return expense.date.getFullYear().toString() === filteredYear;
 	});
 
+	// we can assign JSX to variables
+	let expensesContent = <p>No expenses found</p>;
+
+	if (filteredExpenses.length > 0) {
+		expensesContent = filteredExpenses.map((expense) => (
+			<ExpenseItem
+				key={expense.id}
+				title={expense.title}
+				amount={expense.amount}
+				date={expense.date} />
+		));
+	}
+
 	// note () inside map(=>()) not map(=>{})
 	// always add the key= field so that the React know which components to update in case of change in the array
 	// instead of id, we can use the index as the key (so it will become like: props.items.map((expense,index))
@@ -23,13 +36,27 @@ const Expenses = (props) => {
 		<div>
 			<Card className='expenses'>
 				<ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-				{filteredExpenses.map((expense) => (
+				{/* instead of using the long ternary expression, we can use below 2 standalone expressions */}
+				{/* {filteredExpenses.length === 0 && <p>No expenses found</p>}
+				{filteredExpenses.length > 0 && filteredExpenses.map((expense) => (
 					<ExpenseItem
 						key={expense.id}
 						title={expense.title}
 						amount={expense.amount}
 						date={expense.date} />
-				))}
+				))} */}
+
+				{/* even better below. declare and use variable: expensesContent */}
+				{/* {filteredExpenses.length === 0 ?
+					(<p>No expenses found</p>) :
+					(filteredExpenses.map((expense) => (
+						<ExpenseItem
+							key={expense.id}
+							title={expense.title}
+							amount={expense.amount}
+							date={expense.date} />
+					)))} */}
+				{expensesContent}
 			</Card>
 		</div>
 	);
